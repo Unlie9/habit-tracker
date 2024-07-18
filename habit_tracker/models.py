63038464cf6b5cc15f2018f739b6f1f24bc57ccf
unlie9 <unlie.devops9@gmail.com>
2024-07-18 13:18:47 +0300

    update models and add logic methods (save, completed_with_times_per_day, achieved_habit_is_true)

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -1,5 +1,4 @@
 from django.utils import timezone
-from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
@@ -9,29 +8,52 @@ from django.db import models
 class User(AbstractUser):
     nickname = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return f"{self.nickname} {self.email} (ID: {self.id})"
+
 
 class Habit(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=120)
-    times_per_day = models.IntegerField()
-    days_to_achieve = models.IntegerField()
+    name = models.CharField(max_length=50, editable=False)
+    description = models.CharField(max_length=120, editable=False)
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
 
 
 class UserHabit(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     habit = models.ForeignKey("Habit", on_delete=models.CASCADE)
-    date_finish = models.DateField()
-    current_date = models.DateField()
-    date_of_assign = models.DateField(auto_now_add=True)
+    date_of_assign = models.DateField(auto_now_add=True, null=True, blank=True, editable=False)
 
     def save(self, *args, **kwargs):
         if not self.pk:
-            self.current_date = timezone.now()
-            self.date_finish = self.current_date + timedelta(days=self.habit.days_to_achieve)
+            self.date_of_assign = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user} {self.habit.name} {self.date_of_assign}"
 
 
 class UserHabitDetail(models.Model):
     user_habit = models.OneToOneField("UserHabit", on_delete=models.CASCADE)
-    skip_day = models.BooleanField()
-    achieved_habit = models.BooleanField()
+    times_per_day = models.IntegerField(default=1)
+    days_to_achieve = models.IntegerField(default=21, editable=False, blank=True, null=True)
+    skip_day = models.BooleanField(default=False, editable=False, blank=True, null=True)
+    achieved_habit = models.BooleanField(default=False, editable=False, blank=True, null=True)
     completed_per_day = models.IntegerField()
+
+    def completed_with_times_per_day(self):
+        self.skip_day = self.completed_per_day < self.times_per_day
+
+        if not self.skip_day:
+            self.days_to_achieve -= 1
+
+        self.completed_per_day = 0
+
+        self.user_habit.save()
+        self.user_habit.habit.save()
+
+    def achieved_habit_is_true(self):
+        if self.days_to_achieve == 0:
+            self.achieved_habit = True
+            self.save()
