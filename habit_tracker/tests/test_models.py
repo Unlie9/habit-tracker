1f19b2f4e9ff567e4ba74aae43c2dc4a1c5c1b7d
unlie9 <unlie.devops9@gmail.com>
2024-07-18 13:27:53 +0300

    wrote tests for models where tests models str and methods

---
+++ habit_tracker/tests/test_models.py
@@ -0,0 +1,96 @@
+from django.test import TestCase
+
+from habit_tracker.models import (
+    User,
+    Habit,
+    UserHabit,
+    UserHabitDetail
+)
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            nickname="johnny12",
+            email="johny12@gmail.com",
+            password="John1234"
+        )
+
+        self.habit = Habit.objects.create(
+            name="Morning Boost",
+            description="Start each day with a quick physical routine"
+                        " to energize yourself for the day ahead.",
+        )
+
+        self.user_habit = UserHabit.objects.create(
+            user=self.user,
+            habit=self.habit
+        )
+
+        self.user_habit_detail = UserHabitDetail.objects.create(
+            user_habit=self.user_habit,
+            completed_per_day=3,
+            times_per_day=5,
+            days_to_achieve=10
+        )
+
+    def test_habit_str(self):
+
+        expected = f"{self.habit.name} {self.habit.description}"
+
+        self.assertEqual(str(self.habit), expected)
+
+    def test_user_str(self):
+        expected = f"{self.user.nickname} {self.user.email} (ID: {self.user.id})"
+
+        self.assertEqual(str(self.user), expected)
+
+    def test_user_habit_str(self):
+        expected = (f"{self.user_habit.user} {self.user_habit.habit.name}"
+                    f" {self.user_habit.date_of_assign}")
+        self.assertEqual(str(self.user_habit), expected)
+
+    def test_user_habit_save(self):
+        self.assertIsNotNone(self.user_habit.date_of_assign)
+
+    def test_not_enough_completed_times(self):
+        expected_times_per_day = 5
+        completed = self.user_habit_detail.completed_per_day
+
+        self.assertLess(completed, expected_times_per_day)
+
+    def test_enough_completed_times(self):
+        expected_days_to_achieve = self.user_habit_detail.days_to_achieve
+
+        self.user_habit_detail.completed_with_times_per_day()
+
+        self.assertEqual(expected_days_to_achieve, self.user_habit_detail.days_to_achieve)
+
+    def test_achieved_habit_true(self):
+        self.user_habit_detail.days_to_achieve = 0
+        self.user_habit_detail.achieved_habit_is_true()
+
+        self.assertEqual(True, self.user_habit_detail.achieved_habit)
+
+    def test_achieved_habit_false(self):
+        self.user_habit_detail.days_to_achieve = 14
+        self.user_habit_detail.achieved_habit_is_true()
+
+        self.assertEqual(False, self.user_habit_detail.achieved_habit)
+
+    def test_completed_with_times_per_day_increase_days_to_achieve(self):
+        self.user_habit_detail.completed_per_day = 5
+        initial_days_to_achieve = self.user_habit_detail.days_to_achieve
+        self.user_habit_detail.completed_with_times_per_day()
+        expected_days_to_achieve = initial_days_to_achieve - 1
+
+        self.assertEqual(expected_days_to_achieve, self.user_habit_detail.days_to_achieve)
+
+    def test_completed_with_times_per_day_not_increase_days_to_achieve(self):
+        self.user_habit_detail.completed_per_day = 3
+        initial_days_to_achieve = self.user_habit_detail.days_to_achieve
+        self.user_habit_detail.completed_with_times_per_day()
+        self.assertEqual(initial_days_to_achieve, self.user_habit_detail.days_to_achieve)
+
+
+
