13b73a58f653b992eda021763bfa5f545c1b9647
unlie9 <unlie.devops9@gmail.com>
2024-07-19 15:19:31 +0300

    update models

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
