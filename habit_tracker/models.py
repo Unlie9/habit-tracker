44a8f6cdaefd2b5dc38a1f6d0444df82057fe6e4
unlie9 <unlie.devops9@gmail.com>
2024-07-19 18:44:22 +0300

    update models for better logic

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -24,11 +24,16 @@ class UserHabit(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     habit = models.ForeignKey("Habit", on_delete=models.CASCADE)
     date_of_assign = models.DateField(auto_now_add=True, null=True, blank=True, editable=False)
+    completed_per_day = models.IntegerField(default=0, blank=True, null=True, validators=[MinValueValidator(0)])
+    times_per_day = models.IntegerField(default=1, null=True, blank=True, validators=[MinValueValidator(1)])
 
     def save(self, *args, **kwargs):
         if not self.pk:
             self.date_of_assign = timezone.now()
         super().save(*args, **kwargs)
+
+    def left_to_do(self):
+        return self.times_per_day - self.completed_per_day
 
     def __str__(self):
         return f"{self.user} {self.habit.name} {self.date_of_assign}"
@@ -36,19 +41,17 @@ class UserHabit(models.Model):
 
 class UserHabitDetail(models.Model):
     user_habit = models.OneToOneField("UserHabit", on_delete=models.CASCADE)
-    times_per_day = models.IntegerField(default=1, null=True, blank=True, validators=[MinValueValidator(1)])
     days_to_achieve = models.IntegerField(default=21, blank=True, null=True, validators=[MinValueValidator(1)])
     skip_day = models.BooleanField(default=False, editable=False, blank=True, null=True)
     achieved_habit = models.BooleanField(default=False, editable=False, blank=True, null=True)
-    completed_per_day = models.IntegerField(default=0, blank=True, null=True, validators=[MinValueValidator(0)])
 
     def completed_with_times_per_day(self):
-        self.skip_day = self.completed_per_day < self.times_per_day
+        self.skip_day = UserHabit.completed_per_day < UserHabit.times_per_day
 
         if not self.skip_day:
             self.days_to_achieve -= 1
 
-        self.completed_per_day = 0
+        UserHabit.completed_per_day = 0
 
         self.user_habit.save()
         self.user_habit.habit.save()
@@ -57,3 +60,4 @@ class UserHabitDetail(models.Model):
         if self.days_to_achieve == 0:
             self.achieved_habit = True
             self.save()
+
