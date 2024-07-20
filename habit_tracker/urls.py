c4c4cb570510f8c17d48820ff21b05179e935c48
unlie9 <unlie.devops9@gmail.com>
2024-07-20 19:29:57 +0300

    update urls

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 from habit_tracker.views import (
     index,
     my_profile,
-    HabitsListView, my_habits,
+    HabitsListView, my_habits
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("may_habits", my_habits, name="my-habits"),
+    path("my_habits", my_habits, name="my-habits"),
     path("all_habits/", HabitsListView.as_view(), name="all-habits"),
     path("my_profile/", my_profile, name="my-profile"),
 ]
