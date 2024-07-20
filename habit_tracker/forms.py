8d6ca6b830bbd7c43cd83abfae4f09741c9177b7
unlie9 <unlie.devops9@gmail.com>
2024-07-21 01:37:08 +0300

    create validation for HabitForm create and update

--- habit_tracker/forms.py
+++ habit_tracker/forms.py
@@ -1,5 +1,8 @@
+import re
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from habit_tracker.models import User, Habit
 
@@ -28,3 +31,35 @@ class HabitForm(forms.ModelForm):
     class Meta:
         model = Habit
         fields = ["name", "description"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+        errors = {}
+
+        if not name or not description:
+            return cleaned_data
+
+        if not re.fullmatch(r'[a-zA-Z]+', name):
+            errors['name'] = "Name must contain only letters."
+
+        if not re.fullmatch(r'[a-zA-Z0-9]+', description):
+            errors['description'] = "Description must contain only alphanumeric characters."
+
+        if len(name) > 16:
+            errors['name'] = "Name must be less than 16 characters."
+
+        if len(description) > 40:
+            errors['description'] = "Description must be less than 40 characters."
+
+        if errors:
+            raise ValidationError(errors)
+
+        return cleaned_data
+
+
+class DeleteHabitForm(forms.ModelForm):
+    class Meta:
+        model = Habit
+        fields = []
