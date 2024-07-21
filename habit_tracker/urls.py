62cfd007076e647dd96f6791d0763ea6971f70d9
unlie9 <unlie.devops9@gmail.com>
2024-07-21 14:23:15 +0300

    added new urls

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -10,11 +10,14 @@ from habit_tracker.views import (
     HabitUpdateView,
     HabitDeleteView,
     AssignHabitView,
+    RegisterView,
+    UserHabitDetailUpdateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('register/', RegisterView.as_view(), name='register'),
     path(
         "confirm/update_days_to_achieve/<int:detail_id>/<str:operation>/",
         confirm_operation,
@@ -26,6 +29,7 @@ urlpatterns = [
         name="complete_operation"
     ),
     path("my_habits/", my_habits, name="my-habits"),
+    path("my_habits/<int:pk>/edit/", UserHabitDetailUpdateView.as_view(), name="my-habits-update"),
     path("all_habits/", HabitListView.as_view(), name="all-habits"),
     path("all_habits/create_habit/", HabitCreateView.as_view(), name="create-habit"),
     path("all_habits/<int:pk>/assign_habit/", AssignHabitView.as_view(), name="assign-habit"),
@@ -33,4 +37,5 @@ urlpatterns = [
     path("all_habits/<int:pk>/delete_habit/", HabitDeleteView.as_view(), name="delete-habit"),
     path("my_profile/", my_profile, name="my-profile"),
 ]
+
 app_name = "habit_tracker"
