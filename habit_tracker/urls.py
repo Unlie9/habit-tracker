86631b5faf5e45f002108101d9610d7e57fc1fcf
unlie9 <unlie.devops9@gmail.com>
2024-07-21 22:36:22 +0300

    update urls

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -2,16 +2,15 @@ from django.urls import path, include
 from habit_tracker.views import (
     index,
     my_profile,
-    my_habits,
+    MyHabitsListView,
     confirm_operation,
     complete_operation,
     HabitListView,
     HabitCreateView,
     HabitUpdateView,
-    HabitDeleteView,
-    AssignHabitView,
+    assign_habit_to_user,
     RegisterView,
-    UserHabitDetailUpdateView,
+    UserHabitDetailUpdateView, remove_habit_from_user,
 )
 
 urlpatterns = [
@@ -28,13 +27,13 @@ urlpatterns = [
         complete_operation,
         name="complete_operation"
     ),
-    path("my_habits/", my_habits, name="my-habits"),
+    path("my_habits/", MyHabitsListView.as_view(), name="my-habits"),
     path("my_habits/<int:pk>/edit/", UserHabitDetailUpdateView.as_view(), name="my-habits-update"),
     path("all_habits/", HabitListView.as_view(), name="all-habits"),
     path("all_habits/create_habit/", HabitCreateView.as_view(), name="create-habit"),
-    path("all_habits/<int:pk>/assign_habit/", AssignHabitView.as_view(), name="assign-habit"),
+    path("all_habits/<int:pk>/assign_habit/", assign_habit_to_user, name="assign-habit"),
+    path("all_habits/<int:pk>/remove_from_my_list/", remove_habit_from_user, name="remove-habit"),
     path("all_habits/<int:pk>/update_habit/", HabitUpdateView.as_view(), name="update-habit"),
-    path("all_habits/<int:pk>/delete_habit/", HabitDeleteView.as_view(), name="delete-habit"),
     path("my_profile/", my_profile, name="my-profile"),
 ]
 
